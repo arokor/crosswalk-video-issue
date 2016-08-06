e7dae30e46ee5b0ec1f5342021270356bdc5df50
Aron Kornhall <aron@kornhall.se>
2016-08-06 20:35:33 +0200

    build

--- prj/android/src/com/test/video/VideoActivity.java
+++ prj/android/src/com/test/video/VideoActivity.java
@@ -26,6 +26,7 @@ public class VideoActivity extends XWalkRuntimeActivityBase {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        setRemoteDebugging(true);
         super.onCreate(savedInstanceState);
     }
 
